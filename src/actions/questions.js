fba90997ca961443b74c3927bc26969d6e92934a
Max Boguslavskiy <max.bogus@gmail.com>
2018-10-29 01:39:52 +0300

    - implemented draft actions; - implemented draft reducers.

---
+++ src/actions/questions.js
@@ -0,0 +1,37 @@
+import {saveQuestion} from "../utils/api";
+import {hideLoading, showLoading} from 'react-redux-loading'
+
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+export const TOGGLE_QUESTION = 'TOGGLE_QUESTION';
+export const SAVE_QUESTION = 'SAVE_QUESTION';
+
+function addQuestion(question) {
+    return {
+        type: SAVE_QUESTION,
+        question,
+    }
+}
+
+export function handleAddQuestion(text, optionOneText, optionTwoText) {
+    return (dispatch, getState) => {
+        const {authedUser} = getState();
+
+        dispatch(showLoading());
+        return saveQuestion({
+            text,
+            author: authedUser,
+            optionOneText,
+            optionTwoText
+        })
+            .then((question) => dispatch(addQuestion(question)))
+            .then(() => (hideLoading()))
+    }
+
+}
+
+export function receiveQuestions(questions) {
+    return {
+        type: RECEIVE_QUESTIONS,
+        questions,
+    }
+}
---
+++ src/actions/shared.js
@@ -0,0 +1,20 @@
+import {getInitialData} from "../utils/api";
+import {receiveQuestions} from "./questions";
+import {receiveUsers} from "./users";
+import {setAuthedUser} from "./authedUser";
+import {showLoading, hideLoading} from 'react-redux-loading'
+
+const AUTHED_ID = 'tylermcginnis';
+
+export function handleIniitalData() {
+    return (dispatch) => {
+        dispatch(showLoading());
+        return getInitialData()
+            .then(({users, tweets}) => {
+                dispatch(receiveUsers(users));
+                dispatch(receiveQuestions(questions));
+                dispatch(setAuthedUser(AUTHED_ID));
+                dispatch(hideLoading())
+            })
+    }
+}
