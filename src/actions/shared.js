c24d858ec9a815d507b4126c08e746b966f9c976
Max Boguslavskiy <max.bogus@gmail.com>
2018-10-31 01:26:17 +0300

    - set default status - not authenticated.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -4,7 +4,8 @@ import {receiveUsers} from "./users";
 import {setAuthedUser} from "./authedUser";
 import {showLoading, hideLoading} from 'react-redux-loading'
 
-const AUTHED_ID = 'tylermcginnis';
+// const AUTHED_ID = 'tylermcginnis';
+const AUTHED_ID = null;
 
 export function handleIniitalData() {
     return (dispatch) => {
