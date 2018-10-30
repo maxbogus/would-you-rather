769bf96de2607e95f24e3f0b21e02f3f83906a82
Max Boguslavskiy <max.bogus@gmail.com>
2018-10-31 01:51:51 +0300

    - added active user login to nav bar; - added logout user button to nav bar.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,51 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, {Component, Fragment} from 'react'
+import {NavLink} from 'react-router-dom'
+import connect from "react-redux/es/connect/connect";
 
-export default function Nav () {
-    return (
-        <nav className='nav'>
-            <ul>
-                <li>
-                    <NavLink to='/' exact activeClassName='active'>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/add' activeClassName='active'>
-                        New Question
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/leaderboard' activeClassName='active'>
-                        Leader Board
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
+class Nav extends Component {
+    render() {
+        return (
+            <nav className='nav'>
+                <ul>
+                    <li>
+                        <NavLink to='/' exact activeClassName='active'>
+                            Home
+                        </NavLink>
+                    </li>
+                    {this.props.logged === true
+                        ? <Fragment>
+                            <li>
+                                <NavLink to='/add' activeClassName='active'>
+                                    New Question
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/leaderboard' activeClassName='active'>
+                                    Leader Board
+                                </NavLink>
+                            </li>
+                            <li>
+                                Active User: {this.props.user}
+                            </li>
+                            <li>
+                                <NavLink to='/logout' activeClassName='active'>
+                                    Logout
+                                </NavLink>
+                            </li>
+                        </Fragment>
+                        : null
+                    }
+                </ul>
+            </nav>
+        )
+    }
 }
+
+function mapStateToProps({authedUser}) {
+    return {
+        logged: authedUser !== null,
+        user: authedUser
+    }
+}
+
+export default connect(mapStateToProps)(Nav)
