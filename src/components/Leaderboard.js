970830906d48cdd29ba8cc353a1c33b305a22f5a
Max Boguslavskiy <max.bogus@gmail.com>
2018-11-04 18:45:16 +0300

    - finished leaderboard sorted by questions + answers.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,21 +3,34 @@ import {connect} from 'react-redux'
 
 class Leaderboard extends Component {
     render() {
-        const {questions, users} = this.props;
-        console.log(questions, users);
+        const {users, userIdsSorted} = this.props;
+        let leaderBoard = <span> Loading ... </span>;
+
+        if (userIdsSorted.length > 0) {
+            leaderBoard = userIdsSorted.map((user) => (
+                <li key={users[user].id}>
+                    <img src={users[user].avatarURL} alt={users[user].name}/>
+                    <span>User: {users[user].name}. Asked: {users[user].questions.length}. Answered: {Object.keys(users[user].answers).length}</span>
+                </li>
+            ))
+        }
 
         return (
             <div>
-                Leaderboard
+                <h2 className='center'>Leaderboard</h2>
+                <ul className='dashboard-list'>
+                    {leaderBoard}
+                </ul>
             </div>
         )
     }
 }
 
-function mapsStateToProps({questions, users}) {
+function mapsStateToProps({users}) {
     return {
-        questions,
-        users
+        users,
+        userIdsSorted: Object.keys(users)
+            .sort((a, b) => (users[b].questions.length + Object.keys(users[b].answers).length) - (users[a].questions.length + Object.keys(users[a].answers).length))
     }
 }
 
