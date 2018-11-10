8813aa32cdcca875cc8272717cffbfb6c80e8767
Max Boguslavskiy <max.bogus@gmail.com>
2018-11-10 16:43:40 +0300

    - implemented save question action.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,14 +1,21 @@
-import {saveQuestion} from "../utils/api";
+import {saveQuestion, saveQuestionAnswer} from "../utils/api";
 import {hideLoading, showLoading} from 'react-redux-loading'
 
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
-export const TOGGLE_QUESTION = 'TOGGLE_QUESTION';
+export const ANSWER_QUESTION = 'ANSWER_QUESTION';
 export const SAVE_QUESTION = 'SAVE_QUESTION';
 
 function addQuestion(question) {
     return {
         type: SAVE_QUESTION,
         question,
+    }
+}
+
+function answerQuestion(question) {
+    return {
+        type: ANSWER_QUESTION,
+        question
     }
 }
 
@@ -24,6 +31,19 @@ export function handleAddQuestion(optionOneText, optionTwoText) {
         })
             .then((question) => dispatch(addQuestion(question)))
             .then(() => (hideLoading()))
+    }
+}
+
+export function handleAnswerQuestion(info) {
+    return (dispatch) => {
+        dispatch(answerQuestion(info));
+
+        return saveQuestionAnswer(info)
+            .catch((e) => {
+                console.warn('Error in handleAnswerQuestion', e);
+                dispatch(answerQuestion(info));
+                alert('There was an error answering pole. Try again');
+            })
     }
 }
 
