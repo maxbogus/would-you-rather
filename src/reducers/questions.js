111171020559cd7143e5bc17d3b274f8eb3a6810
Max Boguslavskiy <max.bogus@gmail.com>
2018-11-10 16:42:35 +0300

    - implemented answer question reducer for questions store.

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import {RECEIVE_QUESTIONS, SAVE_QUESTION} from "../actions/questions";
+import {ANSWER_QUESTION, RECEIVE_QUESTIONS, SAVE_QUESTION} from "../actions/questions";
 
 export default function questions(state = {}, action) {
     switch (action.type) {
@@ -12,6 +12,20 @@ export default function questions(state = {}, action) {
             return {
                 ...state,
                 [question.id]: question,
+            };
+        case ANSWER_QUESTION:
+            const {qid, answerType, authedUser} = action.question;
+
+            return {
+                ...state,
+                [qid]: {
+                    ...state[qid],
+                    [answerType]: {
+                        ...state[qid][answerType],
+                        votes:
+                            state[qid][answerType].votes.concat([authedUser])
+                    }
+                }
             };
         default:
             return state;
