df948db531023cf3bbc407ca1fbf65a8e6db2b4b
Max Boguslavskiy <max.bogus@gmail.com>
2018-11-10 16:57:20 +0300

    - implemented answer question reducer for users store.

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,5 @@
 import {RECEIVE_USERS} from "../actions/users";
+import {ANSWER_QUESTION, SAVE_QUESTION} from "../actions/questions"
 
 export default function users(state = {}, action) {
     switch (action.type) {
@@ -6,6 +7,23 @@ export default function users(state = {}, action) {
             return {
                 ...state,
                 ...action.users
+            };
+        case ANSWER_QUESTION:
+            const {authedUser, answerType, qid} = action.question;
+
+            return {
+                ...state,
+                [authedUser]: {
+                    ...state[authedUser],
+                    answers: {
+                        ...state[authedUser].answers,
+                        [qid]: answerType
+                    }
+                }
+            };
+        case SAVE_QUESTION:
+            return {
+                ...state
             };
         default:
             return state;
